2d26218f697a81a9191313e75bfafbce2dced7e1
ShenDezhou <bangtech@sina.com>
2019-03-05 11:11:56 +0800

    Add files via upload
    MainEntry：automodel_batch.py，增加分批训练，将大文件分拆训练后再进行predict，生成结果。
    风险：没把所有数据放到内存一起训练，会不会模型最后只拟合最后训练那一批chunk（param=1000000）数据？

---
+++ automodel_batch.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import numpy
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss, roc_auc_score
+from deepctr.models import DeepFM
+from deepctr import SingleFeat
+import tensorflow as tf
+from keras.callbacks import EarlyStopping
+
+def model_pool(defaultfilename='./input/final_track2_train.txt', defaulttestfile='./input/final_track2_test_no_anwser.txt',
+                defaultcolumnname=['uid', 'user_city', 'item_id', 'author_id', 'item_city', 'channel', 'finish', 'like', 'music_id', 'did', 'creat_time', 'video_duration'],
+                defaulttarget=['finish', 'like'], defaultmodel="AFM", PERCENT=100):
+    
+    data = pd.read_csv(defaultfilename, sep='\t', names=defaultcolumnname, iterator=True)
+    #1 train file
+    loop = True
+    feature_model = False
+    while loop:
+        try:
+            chunk=data.get_chunk(10**7)
+            if PERCENT < 100 and PERCENT > 0:
+                chunk=chunk.take(list(range(min(chunk.shape[0], PERCENT*100))), axis=0)
+
+            if not feature_model:
+                #**************Features
+                sparse_features=[]
+                dense_features=[]
+                target=defaulttarget
+                for column in chunk.columns:
+                    if column in defaulttarget:
+                        continue
+                    if chunk[column].dtype in  [numpy.float_ , numpy.float64]:
+                        dense_features.append(column)
+                    if chunk[column].dtype in [numpy.int_, numpy.int64]:
+                        sparse_features.append(column)
+                
+            
+                #6. generate input data for model
+                sparse_feature_list = [SingleFeat(feat, chunk[feat].nunique())
+                                       for feat in sparse_features]
+                dense_feature_list = [SingleFeat(feat, 0)
+                                      for feat in dense_features]
+                
+                #**************Features
+                
+                #****************model
+                # 6.choose a model
+                import pkgutil
+                import mdeepctr.models
+            #     modelnames = [name for _, name, _ in pkgutil.iter_modules(mdeepctr.__path__)]
+            #     modelname = input("choose a model: "+",".join(modelnames)+"\n")
+            #     if not modelname:
+                modelname=defaultmodel
+                # 7.build a model
+                model = getattr(mdeepctr.models, modelname)({"sparse": sparse_feature_list,
+                                "dense": dense_feature_list}, final_activation='sigmoid', output_dim=len(defaulttarget))
+                # 8. eval predict
+                def auc(y_true, y_pred):
+                    return tf.py_func(roc_auc_score, (y_true, y_pred), tf.double)
+                
+                model.compile("adam", loss="binary_crossentropy", metrics=[auc])
+                #*********************model
+                feature_model = True
+            
+    
+            #***************normal
+            #3. Remove na values
+            chunk[sparse_features] = chunk[sparse_features].fillna('-1', )
+            chunk[dense_features] = chunk[dense_features].fillna(0,)
+            #4. Label Encoding for sparse features, and do simple Transformation for dense features
+            for feat in sparse_features:
+                lbe = LabelEncoder()
+                chunk[feat] = lbe.fit_transform(chunk[feat])
+            #5. Dense normalize
+            if dense_features:
+                mms = MinMaxScaler(feature_range=(0, 1))
+                chunk[dense_features] = mms.fit_transform(chunk[dense_features])
+            #*****************normal
+            
+            train_model_input = [chunk[feat.name].values for feat in sparse_feature_list] + \
+                                [chunk[feat.name].values for feat in dense_feature_list]
+            train_labels = [chunk[target].values for target in defaulttarget]
+
+            my_callbacks = [EarlyStopping(monitor='loss', min_delta=1e-2, patience=3, verbose=1, mode='min')]
+    
+            history = model.fit(train_model_input, train_labels,
+                        batch_size=2**14, epochs=100, verbose=1, callbacks=my_callbacks)
+
+        except StopIteration:
+            loop=False
+            print('stop iteration')
+            
+#     data = pd.concat(take, ignore_index=True, copy=False) 
+#     train_size = data.shape[0]
+#     print(train_size)
+
+    
+    #2 test file       
+    test_data = pd.read_csv(defaulttestfile, sep='\t', names=defaultcolumnname, )
+    #data = data.append(test_data)
+    test_size=test_data.shape[0]
+    print(test_size)
+    #***************normal
+    #3. Remove na values
+    test_data[sparse_features] = test_data[sparse_features].fillna('-1', )
+    test_data[dense_features] = test_data[dense_features].fillna(0,)
+    #4. Label Encoding for sparse features, and do simple Transformation for dense features
+    for feat in sparse_features:
+        lbe = LabelEncoder()
+        test_data[feat] = lbe.fit_transform(test_data[feat])
+    #5. Dense normalize
+    if dense_features:
+        mms = MinMaxScaler(feature_range=(0, 1))
+        test_data[dense_features] = mms.fit_transform(test_data[dense_features])
+    #*****************normal
+    test = test_data
+    test_model_input = [test[feat.name].values for feat in sparse_feature_list] + \
+        [test[feat.name].values for feat in dense_feature_list]
+       
+    pred_ans = model.predict(test_model_input, batch_size=2**14)
+        
+    result = test_data[['uid', 'item_id', 'finish', 'like']].copy()
+    result.rename(columns={'finish': 'finish_probability',
+                           'like': 'like_probability'}, inplace=True)
+    result['finish_probability'] = pred_ans[0]
+    result['like_probability'] = pred_ans[1]
+    output = "%s-result.csv" % (modelname)
+    result[['uid', 'item_id', 'finish_probability', 'like_probability']].to_csv(
+        output, index=None, float_format='%.6f')
+    
+    return history
+
+if __name__ == "__main__":
+    import pkgutil
+    import mdeepctr.models
+    modelnames = [name for _, name, _ in pkgutil.iter_modules(mdeepctr.models.__path__)]
+    functions = ["AFM", "DCN", "MLR",  "DeepFM",
+           "MLR", "NFM", "DIN", "FNN", "PNN", "WDL", "xDeepFM", "AutoInt", ]
+    models_dic = dict((function.lower(),function) for function in functions)
+    for modelname in modelnames:
+        print(modelname)
+        if models_dic[modelname] not in ["PNN"]:
+            continue
+        history = model_pool(defaultmodel=models_dic[modelname], PERCENT=100)
+        print(history.history)
+        
